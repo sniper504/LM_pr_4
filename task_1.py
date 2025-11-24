64af86f301a9f15da0dd956dd604548c8d10d4c5
sniper504 <abracadabra52@tutamail.com>
2025-11-24 14:51:56 +0300

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,21 @@
+from keras import models, layers
+import numpy as np
+
+models = models.Sequential([
+    layers.Dense (3, input_shape = (5,)), activation = 'relu'
+])
+
+weights = np.full((5,3),0.5)
+biases =np.zeros(3)
+models.layers[0].set_weights([w,b])
+x = np.array([1,2,3,4,5], [0,1,0,1,0])
+y = models.predict(x)
+print("Enters:" x)
+print("Outputs:" y)
+weights, biases = models.layers[0].get_weights()
+
+print(biases.shape)
+print(weights.shape)
+print(weights[:3,:5])# матрица весов
+print(biases[:10])# первые 10 сдвигов
+
